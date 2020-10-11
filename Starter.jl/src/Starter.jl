2dd8efc5428896bbdcd72ffc72bdcfb858a21b0e
olayinkaajayi <72391530+olayinkaajayi@users.noreply.github.com>
2020-10-12 00:58:53 +0100

    got working

--- Starter.jl/src/Starter.jl
+++ Starter.jl/src/Starter.jl
@@ -4,6 +4,7 @@ using Plots
 
 # List of functions to be exported
 export example_plot
+export plot_of_f
 
 # Definitions of any new types provided
 
@@ -16,7 +17,7 @@ This function evaluates Sin(x) at n points in the interval [0, 2π], creates a
 """
 function example_plot(n)
     title = "This is an example plot."
-    x1 = collect(range(0.0, 2*π, length=n))
+    x1 = collect(range(0.0, 2*π, length=n)) #This takes the range and make it into an array
     y1 = sin.(x1)
 
     # Plot the points
@@ -24,10 +25,29 @@ function example_plot(n)
     title=title, xlabel="x", ylabel="sin(x)", markersize=10, markercolor="red")
 
     # Now plot the true function
-    x2 = collect(range(0.0, 2*π, length=1000))
+    x2 = collect(range(0.0, 2*π, length=1000)) #This takes the range and make it into an array
     y2 = sin.(x2)
     plot!(x2, y2, label="Underlying function.", color="green", linewidth=2)
     return p
 end
+
+#new function definition
+"""
+    plot_of_f(alpha,n)
+
+This function evaluates (x^alpha)log(x) at n points from 1 to 2^n, creates a plot and returns the plot
+"""
+function plot_of_f(alpha,n)
+    x= [2^i for i=0:n]
+    y= (x.^alpha).*log.(x)
+    p1= plot(x , y,yscale=:log10, seriestype=:scatter, label="Some sample points", xlabel="log(x)",
+        ylabel="(x.^alpha).*log(x)", markersize=10, markercolor="red")
+    x1= collect(range(1,2^21, length= n))
+    y1= log.((x1.^alpha).*log.(x1))
+    plot!(x1, y1, yscale=:log10, label="Underlying function.", color="green", linewidth=2)
+    
+    return p1    
+end
+
 # End the module definition
 end
--- main.jl
+++ main.jl
@@ -12,19 +12,24 @@ args = YAML.load(open("config.yaml"))
 # Print out the arguments and their types
 for key1 in keys(args)
     for key2 in keys(args[key1])
-        value = args[key1][key2]
+        value = args[key1][key2] #This is an array of arrays
         type = typeof(value)
         println("args[", key1, "][", key2, "] = ", value, "  Type = ", type)
     end
 end
 
 # Call a function defined in our custom module Starter.jl
-n = args["group1"]["parameter1"]
+n = args["group1"]["parameter1"] #To use the element one would have to write as a string the label name
 
 # Set default fonts for all plots
 fnt = Plots.font("DejaVu Sans", 10.0)
 default(titlefont=fnt, guidefont=fnt, tickfont=fnt, legendfont=fnt)
-p = example_plot(n)
+p = example_plot(n) #This is the function in our self-tailored package Starter
+
+gr()
+n= args["group3"]["parameter5"]
+alpha= args["group3"]["parameter6"]
+p1= plot_of_f(alpha,n)
 
 # Save the plot as a pdf file
-savefig(p, "testplot.pdf")
+savefig(p, "testplot.pdf") #This saves the plot as a pdf
